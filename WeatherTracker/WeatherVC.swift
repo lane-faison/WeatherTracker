58e9fca5fb4fc881ed8036f841ff1cd7c397182a
Lane Faison <lane.faison@icloud.com>
2017-05-17 09:56:15 -0600

    Front End UI set up

---
+++ WeatherTracker/WeatherVC.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherVC.swift
+//  WeatherTracker
+//
+//  Created by Lane Faison on 5/17/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    
+    }
+
+}
+
