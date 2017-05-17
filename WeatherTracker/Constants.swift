8d3272c9444bf4f7b32bcea8d4d01a8a7eaebec4
Lane Faison <lane.faison@icloud.com>
2017-05-17 13:01:29 -0600

    Constant and CurrentWeather files added. API Key set up.

---
+++ WeatherTracker/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  WeatherTracker
+//
+//  Created by Lane Faison on 5/17/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "7b0b336449c47612c55a5c07fef43dcd"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)40.014984\(LONGITUDE)-105.270546\(APP_ID)\(API_KEY)"
+
+
---
+++ WeatherTracker/CurrentWeather.swift
@@ -0,0 +1,53 @@
+//
+//  CurrentWeather.swift
+//  WeatherTracker
+//
+//  Created by Lane Faison on 5/17/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    
+    // Data encapsulation is a way to make your code safe
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+    
+}
